027377484fa9575a26cbabba031d0a7d6f1ef731
alimasudd-mac <alimasudd.dev@gmail.com>
2024-04-08 15:23:41 +0700

    create theme_provider.dart

---
+++ lib/provider/theme_provider.dart
@@ -0,0 +1,25 @@
+import 'package:bmi_calculator/service/theme_preferences.dart';
+import 'package:flutter/material.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  late bool _isDark;
+  late ThemePreferences themePreferences;
+  bool get isDark => _isDark;
+
+  ThemeProvider() {
+    _isDark = false;
+    themePreferences = ThemePreferences();
+    getThemePreferences();
+  }
+
+  set isDark(bool value) {
+    _isDark = value;
+    themePreferences.setTheme(value);
+    notifyListeners();
+  }
+
+  getThemePreferences() async {
+    _isDark = await themePreferences.getTheme();
+    notifyListeners();
+  }
+}
