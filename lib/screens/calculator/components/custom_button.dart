f060e166fdcd368a0169d73b95e53bbc87986a3a
alimasudd-mac <alimasudd.dev@gmail.com>
2024-04-16 10:08:13 +0700

    create custom_button.dart

---
+++ lib/screens/calculator/components/custom_button.dart
@@ -0,0 +1,50 @@
+import 'package:bmi_calculator/constants/colors.dart';
+import 'package:bmi_calculator/provider/theme_provider.dart';
+import 'package:flutter/material.dart' hide BoxDecoration, BoxShadow;
+import 'package:flutter_inset_box_shadow/flutter_inset_box_shadow.dart';
+import 'package:provider/provider.dart';
+import 'package:sizer/sizer.dart';
+import '../../../provider/bmi_provider.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key, required this.label});
+
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<Mass>(context);
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    final theme = Theme.of(context);
+    return GestureDetector(
+      onTap: () {
+        if (provider.weight.length < 3) {
+          provider.kilo(label);
+        }
+
+        if (provider.height.length < 3) {
+          provider.inches(label);
+        } else {
+          return;
+        }
+      },
+      child: Container(
+        height: 10.h,
+        width: 20.w,
+        decoration: BoxDecoration(
+          color: theme.cardColor,
+          boxShadow: themeProvider.isDark
+              ? AppColors.darkOuterBoxShadow
+              : AppColors.outerBoxShadow,
+          shape: BoxShape.circle,
+        ),
+        child: Center(
+            child: Text(label,
+                style: TextStyle(
+                    fontSize: 24.sp,
+                    color: theme.primaryColor,
+                    fontWeight: FontWeight.bold))),
+      ),
+    );
+  }
+}
