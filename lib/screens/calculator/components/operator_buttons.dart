758e861b730d9b336eb26fd2b4e1bbeaa3e9ad70
alimasudd-mac <alimasudd.dev@gmail.com>
2024-04-17 14:33:17 +0700

    create operator_buttons.dart

---
+++ lib/screens/calculator/components/operator_buttons.dart
@@ -0,0 +1,54 @@
+import 'package:bmi_calculator/constants/app_size.dart';
+import 'package:bmi_calculator/constants/colors.dart';
+import 'package:bmi_calculator/provider/theme_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sizer/sizer.dart';
+
+class OperatorButton extends StatelessWidget {
+  final VoidCallback onTap;
+  final String label;
+  final bool isIconData;
+  const OperatorButton(
+      {super.key,
+      required this.onTap,
+      this.label = "",
+      this.isIconData = false});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: AppSize.operatorHeight,
+        width: AppSize.operatorWidth,
+        decoration: BoxDecoration(
+          color: theme.cardColor,
+          boxShadow: themeProvider.isDark
+              ? AppColors.darkOuterBoxShadow
+              : AppColors.outerBoxShadow,
+          borderRadius: const BorderRadius.all(
+            Radius.circular(40),
+          ),
+        ),
+        child: isIconData
+            ? Center(
+                child: Icon(
+                  Icons.arrow_back,
+                  size: 5.h,
+                  color: AppColors.secondaryColor,
+                ),
+              )
+            : Center(
+                child: Text(label,
+                    style: TextStyle(
+                        fontSize: 20.sp,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.secondaryColor)),
+              ),
+      ),
+    );
+  }
+}
