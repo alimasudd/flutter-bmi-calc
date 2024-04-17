dde9b7526b989499f39eb51368b2c05718b173fa
alimasudd-mac <alimasudd.dev@gmail.com>
2024-04-17 14:33:52 +0700

    create home_screen.dart

---
+++ lib/screens/calculator/home_screen.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+import 'package:bmi_calculator/screens/calculator/components/body.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Future<void> timeDelayed;
+  @override
+  void initState() {
+    super.initState();
+    timeDelayed = Future.delayed(const Duration(milliseconds: 1600));
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      body: SlideTransition(
+          position: Tween<Offset>(begin: const Offset(0, -1), end: Offset.zero)
+              .animate(animationController),
+          child: const Body()),
+    );
+  }
+}
