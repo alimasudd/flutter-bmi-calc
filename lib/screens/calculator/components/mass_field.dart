1158a447f8d9ab5a9277b4b4fe0bc13223fb6923
alimasudd-mac <alimasudd.dev@gmail.com>
2024-04-17 14:33:01 +0700

    create mass_field.dart

---
+++ lib/screens/calculator/components/mass_field.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:bmi_calculator/screens/calculator/components/height_field.dart';
+import 'package:bmi_calculator/screens/calculator/components/weight_field.dart';
+import 'package:flutter/material.dart' hide BoxDecoration, BoxShadow;
+import 'package:sizer/sizer.dart';
+
+class MassField extends StatefulWidget {
+  const MassField({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MassField> createState() => _MassFieldState();
+}
+
+class _MassFieldState extends State<MassField> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        WeightField(),
+        SizedBox(
+          height: 3.h,
+        ),
+        HeightField()
+      ],
+    );
+  }
+}
