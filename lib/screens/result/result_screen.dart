dde5943207894cecc7fe0dd16c99ad63f6dcbbad
alimasudd-mac <alimasudd.dev@gmail.com>
2024-04-18 08:52:13 +0700

    create result_screen.dart

---
+++ lib/screens/result/result_screen.dart
@@ -0,0 +1,14 @@
+import 'package:bmi_calculator/screens/result/components/body.dart';
+import 'package:flutter/material.dart';
+
+class ResultScreen extends StatelessWidget {
+  const ResultScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      body: const Body(),
+    );
+  }
+}
