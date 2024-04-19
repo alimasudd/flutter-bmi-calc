91fe60a3a343a3971a10b8de449f33df6f934c00
alimasudd-mac <alimasudd.dev@gmail.com>
2024-04-19 08:52:23 +0700

    create custom_meter.dart

---
+++ lib/screens/result/components/custom_meter.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+class CustomMeter extends StatelessWidget {
+  const CustomMeter({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text(
+              "Underweight",
+              style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12.sp),
+            ),
+            Text(
+              "Normal",
+              style: TextStyle(
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12.sp),
+            ),
+            Text(
+              "Overweight",
+              style: TextStyle(
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12.sp),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 1.h,
+        ),
+        ClipRRect(
+          borderRadius: const BorderRadius.all(Radius.circular(25)),
+          child: SizedBox(
+            height: 1.2.h,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    color: Colors.blue,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.green,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.red,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 1.h,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("16.0",
+                style: TextStyle(fontSize: 11.sp, color: theme.primaryColor)),
+            Text("18.5",
+                style: TextStyle(fontSize: 11.sp, color: theme.primaryColor)),
+            Text("25.0",
+                style: TextStyle(fontSize: 11.sp, color: theme.primaryColor)),
+            Text("30.0",
+                style: TextStyle(fontSize: 11.sp, color: theme.primaryColor)),
+          ],
+        ),
+      ],
+    );
+  }
+}
